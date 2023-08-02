09749120dd4ea333d056172e789f3263a7a6ada8
DsplGroup <141230655+dsplEnsblFtrRnkgUn@users.noreply.github.com>
2023-08-02 20:05:48 +0330

    Delete ABS-dspl/SmartHome directory

--- ABS-dspl/SmartHome/StateUpdate_i.java
+++
@@ -1,9 +0,0 @@
-package SmartHome;
-// reconf05.abs:9:0: 
-public interface StateUpdate_i extends org.abs_models.backend.java.lib.types.ABSInterface, ABS.StdLib.Object_i {
-    // reconf05.abs:9:24: 
-    public  org.abs_models.backend.java.lib.runtime.ABSFut<org.abs_models.backend.java.lib.types.ABSString> async_getName();
-    // reconf05.abs:9:24: 
-    public  org.abs_models.backend.java.lib.types.ABSString getName();
-    boolean __ABS_isSameCOG();
-}
