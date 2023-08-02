da2da73ca55665249d82869ed9f3f8cb2d49f6bf
DsplGroup <141230655+dsplEnsblFtrRnkgUn@users.noreply.github.com>
2023-08-02 20:09:40 +0330

    Delete ABS-dspl/gen/SmartHome directory

--- ABS-dspl/gen/SmartHome/ABS/DC/sumDivsN_f.java
+++
@@ -1,8 +0,0 @@
-package ABS.DC;
-// abs/lang/abslang.abs:781:0: 
-public final class sumDivsN_f implements org.abs_models.backend.java.lib.types.ABSFunction {
-    private sumDivsN_f() { }
-    public static org.abs_models.backend.java.lib.types.ABSRational apply(ABS.StdLib.List<org.abs_models.backend.java.lib.types.ABSRational> consumeds, ABS.StdLib.List<org.abs_models.backend.java.lib.types.ABSRational> totals, org.abs_models.backend.java.lib.types.ABSInteger n) {
-        return (org.abs_models.backend.java.lib.expr.BinOp.eq(n,org.abs_models.backend.java.lib.types.ABSInteger.fromLong(0L)).or(ABS.StdLib.isEmpty_f.apply(consumeds)).or(ABS.StdLib.isEmpty_f.apply(totals)).toBoolean() ? org.abs_models.backend.java.lib.types.ABSInteger.fromLong(0L) : ABS.StdLib.head_f.apply(consumeds).multiply(org.abs_models.backend.java.lib.types.ABSInteger.fromLong(100L)).divide(ABS.StdLib.head_f.apply(totals)).add(ABS.DC.sumDivsN_f.apply(ABS.StdLib.tail_f.apply(consumeds), ABS.StdLib.tail_f.apply(totals), n.subtract(org.abs_models.backend.java.lib.types.ABSInteger.fromLong(1L)))));
-    }
-}
--- ABS-dspl/gen/SmartHome/Runtime_Product_i.java
+++
@@ -1,9 +0,0 @@
-package SmartHome;
-// reconf05.abs:3:0: 
-public interface Runtime_Product_i extends org.abs_models.backend.java.lib.types.ABSInterface, ABS.StdLib.Object_i {
-    // reconf05.abs:3:27: 
-    public  org.abs_models.backend.java.lib.runtime.ABSFut<org.abs_models.backend.java.lib.types.ABSUnit> async_run();
-    // reconf05.abs:3:27: 
-    public  org.abs_models.backend.java.lib.types.ABSUnit run();
-    boolean __ABS_isSameCOG();
-}
